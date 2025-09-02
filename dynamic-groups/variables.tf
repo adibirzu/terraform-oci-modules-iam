d4d03bec6898aa1b87dc58de5e4e96fc67ca8717
Adi Mac <adibirzu@gmail.com>
2025-09-02 21:43:04 +0300

    Update variables.tf

--- dynamic-groups/variables.tf
+++ dynamic-groups/variables.tf
@@ -1,28 +1,20 @@
-# Copyright (c) 2023 Oracle and/or its affiliates.
-# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
-
 variable "tenancy_ocid" {
+  type        = string
   description = "The OCID of the tenancy."
 }
 
 variable "dynamic_groups_configuration" {
-  description = "The dynamic groups."
-  type = object({
-    default_defined_tags  = optional(map(string)),
-    default_freeform_tags = optional(map(string))
-    dynamic_groups = map(object({
-      name          = string,
-      description   = string,
-      matching_rule = string
-      defined_tags  = optional(map(string)),
-      freeform_tags = optional(map(string))
-    }))
-  })
-  default = null
+  type = any
+  default = {
+    "default_defined_tags" : null,
+    "default_freeform_tags" : {
+      "ManagedBy" : "CIS-LZ"
+    },
+    "dynamic_groups" : {}
+  }
 }
 
 variable "module_name" {
-  description = "The module name."
-  type        = string
-  default     = "iam-dynamic-groups"
+  type    = string
+  default = "cis-landing-zone-iam-dynamic-groups"
 }
